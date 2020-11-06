150e318c7a1a8861503742a4084fc04c58c4589b
dj-Andres <andres96jimenez@gmail.com>
2020-11-05 20:57:27 -0500

    modficando el contenido

--- views/template/barra.php
+++ views/template/barra.php
@@ -1,18 +1,17 @@
-<?php
-session_start();
-    $_SESSION['id_usuario'];
-?>
-
 <div class="h-header">
-    <div class="h-logo"><a href="home.php"><img src="img/logo.png" width="130"></a></div>
-    <div class="h-search"><input type="text" placeholder="Busca" class="search"></div>
-    <div class="h-account">
-        <a href=""><img src="img/icons/explorar.png" class="i-icon"></a>
-        <a href="perfil.php?username=<?php echo $_SESSION['usuario']; ?>">
-            <img src="img/icons/perfil.png" class="i-icon">
-        </a>
-        <a href="../controllers/controller-logout.php">
-            <img src="img/icons/close.png" class="i-icon" width="24px">
-        </a>
-    </div>
+  <div class="h-logo"><a href="home.php"><img src="../img/logo.png" width="130"></a></div>
+  <div class="h-search">
+    <form action="">
+        <input type="text" placeholder="Busca" class="search" id="Buscar">
+    </form>
+  </div>
+  <div class="h-account">
+    <a href=""><img src="../img/icons/explorar.png" class="i-icon"></a>
+    <a href="perfil.php?username=<?php echo $_SESSION['usuario']; ?>">
+      <img src="../img/icons/perfil.png" class="i-icon">
+    </a>
+    <a href="../controllers/controller-logout.php">
+      <img src="../img/icons/close.png" class="i-icon" width="24px">
+    </a>
+  </div>
 </div>
